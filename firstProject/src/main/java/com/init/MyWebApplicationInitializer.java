6bed1138eb028917695c7683849f33d5d310d485
btf <1967980236@qq.com>
2021-11-05 08:15:34 +0800

    第二个工程

---
+++ firstProject/src/main/java/com/init/MyWebApplicationInitializer.java
@@ -0,0 +1,24 @@
+package com.init;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		// context.register();
+		DispatcherServlet servlet = new DispatcherServlet(context);
+		Dynamic registration = servletContext.addServlet( "app",servlet);
+		
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/app/*");
+	}
+
+}

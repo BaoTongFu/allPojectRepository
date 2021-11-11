186ab93cd11ec189f664e3a3e53898b42591bcf5
btf <1967980236@qq.com>
2021-11-11 13:17:06 +0800

    提交

--- firstProjectWebXmlConfigure/src/main/java/com/btf/online/web/supperadmin/DepartmentController.java
+++ firstProjectWebXmlConfigure/src/main/java/com/btf/online/web/supperadmin/DepartmentController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.btf.online.entity.Department;
 import com.btf.online.service.DepartmentService;
 
+
 @Controller
 @RequestMapping("/superadmin")
 public class DepartmentController {
+	Logger logger = LoggerFactory.getLogger(DepartmentController.class);
 	@Autowired
 	private DepartmentService departmentService;
 	@RequestMapping(value="listdepartment",method=RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> queryDepartment(){
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		List<Department> list = new ArrayList<>();
 		try {
@@ -33,6 +39,10 @@ public class DepartmentController {
 			modelMap.put("success", false);
 			modelMap.put("erMsg", e.toString());
 		}
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime - startTime);
+		logger.info("===end===");
 		return modelMap;
 	}
 
